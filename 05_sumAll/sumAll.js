9f7fcf6f28b52708c963d3ff36c7b386f56d8e97
David Delgado <david.delgadop88@gmail.com>
2024-02-16 09:32:17 -0400

    Solved ex 1 to 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(startNumber, endNumber) {
+    if (startNumber < 0 || endNumber < 0) {
+        return "ERROR";
+    }
 
+    if (typeof startNumber !== "number" || typeof endNumber !== "number") {
+        return "ERROR";
+    }
+
+    let sum = 0;
+
+    if (endNumber < startNumber) {
+        let aux = startNumber;
+        startNumber = endNumber;
+        endNumber = aux;
+    }
+
+    for (let i = startNumber; i <= endNumber; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
