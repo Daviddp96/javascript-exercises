2a54c120d870446e698a76745067a3acb15b7f51
David Delgado <david.delgadop88@gmail.com>
2024-02-17 14:31:54 -0400

    Solved ex 08 to 11

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    const parsedN = parseInt(n);
 
+    if (parsedN < 0) {
+        return "OOPS";
+    }
+    if (parsedN == 0) {
+        return 0;
+    }
+    if (parsedN <= 2) {
+        return 1;
+    }
+
+    let lastToPrev = 1;
+    let prev = 1;
+    let current = 0;
+    for (let i = 2; i < parsedN; i++) {
+        current = lastToPrev + prev;
+        lastToPrev = prev;
+        prev = current;
+    }
+    return current;
 };
 
 // Do not edit below this line
